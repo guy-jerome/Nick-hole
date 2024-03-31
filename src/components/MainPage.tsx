bfc1957fff4daccf7375d090a339a3eed03dad2e
guy-jerome <aaronjamesroberts@gmail.com>
2024-03-31 18:36:55 -0400

    Added a hat tracking code, that still is in beta

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,9 +1,39 @@
+import { useState } from "preact/hooks";
+
 export default function MainPage() {
+  const handleDragStart = (event: any) => {
+    event.dataTransfer.setData("text/plain", event.target.id);
+  };
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e) => {
+    const imageWidth = 500; // Set this to the actual width of your image
+    const imageHeight = 500; // Set this to the actual height of your image
+    setPosition({
+      x: e.clientX + imageWidth / 2,
+      y: e.clientY - imageHeight / 2,
+    });
+    setPosition({ x: e.clientX, y: e.clientY });
+  };
   return (
-    <div id="main-page">
+    <div id="main-page" onMouseMove={handleMouseMove}>
       <h1>Welcome to the Nick-Hole</h1>
       <p>This is the land of everything Nicky!</p>
-      <img src="src/assets/Mug.jpg"></img>
+      <img className="image" src="src/assets/Mug.jpg" alt="Mug" id="mug"></img>
+      <img
+        className="clothing tophat"
+        src="src/assets/tophat.png"
+        alt="Top Hat"
+        id="tophat"
+        onDragStart={handleDragStart}
+        style={{
+          position: "absolute",
+          top: position.y,
+          left: position.x,
+          width: "100px",
+          height: "100px",
+        }}
+      ></img>
     </div>
   );
 }
